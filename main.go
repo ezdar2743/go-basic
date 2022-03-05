8abc446cec83608408da1cd044c38f5190fed9c2
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-05 16:44:22 +0900

    defer and naked return

--- main.go
+++ main.go
@@ -5,15 +5,24 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
+func lenAndUpper(name string) (length int, upper string) {
+	length = len(name)
+	upper = strings.ToUpper(name)
+	return
+} //naked return
+func lenAndUpper2(name string) (int, string) {
+	defer fmt.Println("I'm done")
 	return len(name), strings.ToUpper(name)
-}
+} // nomal return
+
 func repeatMe(words ...string) {
+	defer fmt.Println("I'm done")
 	fmt.Println(words)
 }
 
 func main() {
-	totalLength, upperName := lenAndUpper("seokkku")
-	fmt.Println(totalLength, upperName)
-	repeatMe("a", "b", "c", "d")
+	totalLen, upper := lenAndUpper2("nwaaw")
+	fmt.Println(totalLen, upper)
+	repeatMe("ss", "ss2")
+
 }
